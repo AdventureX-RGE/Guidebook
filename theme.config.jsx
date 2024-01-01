bb09ee2d40df07a68cfed4f2798ac396f30fb932
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2024-01-01 20:25:08 +0800

    add 4 categories & meta data update

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,28 @@
+import { useRouter } from 'next/router'
+
 export default {
-  logo: <span>My Nextra Documentation</span>,
+  logo: <span>AdventureX Guidebook</span>,
   project: {
-    link: 'https://github.com/shuding/nextra'
+    link: 'https://github.com/AdventureX-RGE/Guidebook'
+  },
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – AdventureX'
+      }
+    }
+  },
+  sidebar: {
+    titleComponent({ title, type }) {
+      if (type === 'separator') {
+        return <span className="cursor-default">{title}</span>
+      }
+      return <>{title}</>
+    },
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true,
+    autoCollapse: true
   },
   // ... other theme options
   navigation: true
